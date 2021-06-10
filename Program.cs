c87b1255605f545287d0a2111f581ddfa83bfcd7
dlozina <dino.lozina@itsistemi.com>
2021-06-10 14:15:19 +0200

    admin - test return Http client builder

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ namespace DevPage
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
             await builder.Build().RunAsync();
         }
     }
