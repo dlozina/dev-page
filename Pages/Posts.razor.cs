11fb7a89b71376e67e1b6e34939b014d3ee58381
dlozina <dlozina@infobip.com>
2021-10-14 09:47:55 +0200

    Changed post article location to devto

--- Pages/Posts.razor.cs
+++ Pages/Posts.razor.cs
@@ -9,6 +9,6 @@ namespace DevPage.Pages
     {
         private string _postTitle1 = "Web Api Broadcasting";
         private string _postDescription1 = "Shared code between Server and Client App, Live updates(Signal R), Web Api Broadcasting, Client Blazor WASM receives notifications.";
-        private string _publishDate = "Published: 24.05.2021";
+        private string _publishDate = "Published: 14.10.2021";
     }
 }
