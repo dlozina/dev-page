9a8c140dde2307797401df8f4dc9a9fc787d97c4
Dino Lozina <Dino.Lozina@infobip.com>
2021-11-18 18:41:20 +0100

    page content update

--- Pages/Posts.razor.cs
+++ Pages/Posts.razor.cs
@@ -16,5 +16,10 @@ namespace DevPage.Pages
         private string _postTitle2 = "How to write URL shortener in .NET5?";
         private string _postDescription2 = "In this series we will try to show you how easy it is to write a service that shortens URLs with .NET.";
         private string _publishDate2 = "Published: 11.11.2021";
+        
+        // Post 3
+        private string _postTitle3 = "How to schedule tasks in .NET?";
+        private string _postDescription3 = "Scheduling jobs in applications can be a really daunting work but it doesn't have to";
+        private string _publishDate3 = "Published: 11.11.2021";
     }
 }
